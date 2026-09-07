a155806c9a7d32e89c0fa23109379345443eceb8
Héctor Duarte <50688196+hdulam@users.noreply.github.com>
2026-09-06 23:41:14 -0600

    Completa funcional

--- app/src/main/java/com/example/cafechapin/MainActivity.kt
+++ app/src/main/java/com/example/cafechapin/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.cafechapin.navigation.StoreNavigation
+import com.example.cafechapin.ui.StoreViewModel
 import com.example.cafechapin.ui.theme.CafeChapinTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CafeChapinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val storeViewModel: StoreViewModel = viewModel()
+                val uiState = storeViewModel.uiState.collectAsStateWithLifecycle().value
+
+                StoreNavigation(
+                    uiState = uiState,
+                    onToggleFavorite = storeViewModel::toggleFavorite
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CafeChapinTheme {
-        Greeting("Android")
     }
 }
